104b7a9b9ef03329561e396dad2da86f7f824d56
wenzongteo <wenzongteo@gmail.com>
2017-10-18 12:25:31 +0800

    test 5

--- src/main/java/seedu/address/model/person/Photo.java
+++ src/main/java/seedu/address/model/person/Photo.java
@@ -28,14 +28,16 @@ public class Photo {
     public Photo(String photo) throws IllegalValueException {
         photo = photo.trim();
 
-        if (!isValidPhoto(photo) || new File(photo).exists()) {
+        if (!isValidPhoto(photo)) {
             throw new IllegalValueException(MESSAGE_PHOTO_CONSTRAINTS);
         } else {
             File dp = new File(photo);
-            if (!dp.exists()) {
+            if (dp.exists()) {
+                this.value = photo;
+            } else {
+                throw new IllegalValueException(MESSAGE_PHOTO_NOT_FOUND);
             }
         }
-        this.value = photo;
     }
 
     /**
