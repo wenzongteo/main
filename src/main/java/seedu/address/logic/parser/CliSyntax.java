44f602ed84cfddaa00d0f6874eecbc36afa01041
Bryan <bryankoh137@hotmail.com>
2017-10-19 18:48:19 +0800

    Updated CliSyntax.java

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -13,5 +13,5 @@ public class CliSyntax {
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_SORT = new Prefix("s/");
     public static final Prefix PREFIX_PHOTO = new Prefix("dp/");
-    
+
 }
