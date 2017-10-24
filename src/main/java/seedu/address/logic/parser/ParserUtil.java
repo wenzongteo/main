a4445ff6ce10f47a85e0e2209d97771c0407c90f
wenzongteo <wenzongteo@gmail.com>
2017-10-24 16:57:35 +0800

    update necessary changes

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -106,7 +106,7 @@ public class ParserUtil {
      */
     public static Optional<Photo> parsePhoto(Optional<String> photo) throws IllegalValueException {
         requireNonNull(photo);
-        return photo.isPresent() ? Optional.of(new Photo(photo.get())) : Optional.of(new Photo("default.jpeg"));
+        return photo.isPresent() ? Optional.of(new Photo(photo.get())) : Optional.of(new Photo("data/images/default.jpeg"));
     }
 
     /**
