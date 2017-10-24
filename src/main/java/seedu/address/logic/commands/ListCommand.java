22e7c92716aa201340bee18086182d016cc1fca5
Hengyu <limhengyu95@gmail.com>
2017-10-24 19:34:31 +0800

    Updated style

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,8 +1,6 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SORT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 
@@ -13,7 +11,6 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
     public static final String COMMAND_ALIAS = "l";
-    private int sortOrder = 0;
     public static final String MESSAGE_SUCCESS = "Listed all persons";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all the contacts and if specified, sort by tags "
             + "or by the names in alphabetical order\n"
@@ -22,6 +19,8 @@ public class ListCommand extends Command {
             + "Examples:\n"
             + "1) " + COMMAND_WORD + "\n"
             + "2) " + COMMAND_WORD + " " + PREFIX_SORT + "tag";
+
+    private int sortOrder = 0;
 
     public ListCommand() {
         new ListCommand(0);
