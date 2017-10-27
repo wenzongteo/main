f86cd5530044216d8b4fa17bc4091859158f914f
wenzongteo <wenzongteo@gmail.com>
2017-10-27 09:01:52 +0800

    update test case

--- src/test/java/seedu/address/model/person/PhotoTest.java
+++ src/test/java/seedu/address/model/person/PhotoTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import seedu.address.commons.exceptions.IllegalValueException;
+
 public class PhotoTest {
 
     @Test
@@ -27,5 +29,39 @@ public class PhotoTest {
         assertTrue(Photo.isValidPhoto("docs/images/yl_coder.jpg")); // end with .jpg file extension
         assertTrue(Photo.isValidPhoto("docs/images/yl_coder.jpg")); // file name with special character _
         assertTrue(Photo.isValidPhoto("docs/images/prof damith.jpg")); //file name with space in between
+    }
+
+    @Test
+    public void photoHashCode() {
+        try {
+            Photo photo1 = new Photo("default.jpeg",0);
+            Photo photo2 = new Photo("data/images/alice@example.com.jpg");
+
+            //hashcode matches for same email --> return true
+            assertTrue(photo1.hashCode() == photo1.hashCode());
+
+            //hashcode don't match for different email --> return false
+            assertFalse(photo1.hashCode() == photo2.hashCode());
+
+        } catch (IllegalValueException e) {
+            assert false : "shouldn't hit this case at all";
+        }
+    }
+
+    @Test
+    public void compareHash() {
+        try {
+            Photo photo1 = new Photo("default.jpeg",0);
+            Photo photo2 = new Photo("docs/images/wz.jpg");
+
+            //Compare hash of the same photo --> return true
+            assertTrue(photo1.getHash().equals(photo1.getHash()));
+
+            //Compare hash of different photos --> return false
+            assertFalse(photo1.getHash().equals(photo2.getHash()));
+
+        } catch (IllegalValueException e) {
+            assert false : "Not possible";
+        }
     }
 }
