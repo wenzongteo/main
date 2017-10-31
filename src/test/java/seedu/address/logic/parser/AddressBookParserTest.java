686a3de380da1241edf9d146934ea938fbc3b432
wenzongteo <wenzongteo@gmail.com>
2017-10-31 19:52:29 +0800

    update checkstyle

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -42,6 +42,10 @@ import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.PersonUtil;
 
 public class AddressBookParserTest {
+    private final AddressBookParser parser = new AddressBookParser();
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @BeforeClass
     public void setup() {
         try {
@@ -53,11 +57,6 @@ public class AddressBookParserTest {
             throw new AssertionError("Impossible, no other exceptions should be thrown!!");
         }
     }
-
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    private final AddressBookParser parser = new AddressBookParser();
 
     @Test
     public void parseCommand_add() throws Exception {
