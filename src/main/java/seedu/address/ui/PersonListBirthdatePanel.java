fa6ad7cf8fe55757cfa4271df6370498aa36ac7b
ritchielq <ritchie_freezie@hotmail.com>
2017-11-05 23:50:06 +0800

    Removed unused imports

--- src/main/java/seedu/address/ui/PersonListBirthdatePanel.java
+++ src/main/java/seedu/address/ui/PersonListBirthdatePanel.java
@@ -5,8 +5,6 @@ import java.util.logging.Logger;
 
 import org.fxmisc.easybind.EasyBind;
 
-import com.google.common.eventbus.Subscribe;
-
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -17,8 +15,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
-import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
 import seedu.address.model.person.ReadOnlyPerson;
 
 //@@author hengyu95
