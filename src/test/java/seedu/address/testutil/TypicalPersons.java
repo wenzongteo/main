e8051c13cd4545ef87144f941ddfa03202cd9b56
wenzongteo <wenzongteo@gmail.com>
2017-11-10 07:41:24 +0800

    update checkstyle

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -86,8 +86,8 @@ public class TypicalPersons {
             .withEmailAddress("missingname@example.com").withPhoto("data/images/hans@example.com.jpg")
             .withAddress("chicago ave").build();
     public static final ReadOnlyPerson MISSINGADDRESS = new PersonBuilder().withName("Ida Mueller")
-            .withPhone("84822131").withEmailAddress("missingaddress@example.com").withPhoto("data/images/hans@example.com.jpg")
-            .build();
+            .withPhone("84822131").withEmailAddress("missingaddress@example.com")
+            .withPhoto("data/images/hans@example.com.jpg").build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
